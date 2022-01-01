1e82592661a298f0cf5f97f20077240b22662cc9
Özgür Odabaşı <ozgurodabasi@gmail.com>
2022-01-01 23:10:05 +0300

    - Package dependencies fixed

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "WLM3U",
-    platforms: [.iOS(.v10),],
+    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v14)],
     products: [.library(name: "WLM3U", targets: ["WLM3U"])],
     dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.1.0"))],
     targets: [.target(name: "WLM3U", dependencies: ["Alamofire"], path: "Sources")]
